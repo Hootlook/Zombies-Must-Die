fcf9a3702bb473665d8e53560aa4f119026fc19f
Maélie Lenard <maelie.lenard@gmail.com>
2019-04-04 09:55:26 +0200

    ajout d un package simple temple

---
+++ Zombies Must Die/Assets/PreProd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreProd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // PREPROD
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
